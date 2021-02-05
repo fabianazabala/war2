33b8c8a89124115f6dc02b3eb7491a484c576540
fabiana.zabala <fabianazabala@hotmail.es>
2021-02-05 23:19:23 +0100

    Implemented tests for the InputHandler class.

--- src/test/java/com/epam/war/service/InputHandlerTest.java
+++ src/test/java/com/epam/war/service/InputHandlerTest.java
@@ -1,7 +1,63 @@
 package com.epam.war.service;
 
-import static org.testng.Assert.*;
+import static com.github.stefanbirkner.systemlambda.SystemLambda.catchSystemExit;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+
+import com.epam.war.domain.DeckSize;
+import com.epam.war.service.screen.Usage;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class InputHandlerTest {
 
+  @Mock
+  Usage usage;
+
+  @BeforeMethod
+  public void beforeMethod() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void inputHasInvalidNumberOfArguments_thenMessageIsPrinted() throws Exception {
+    InputHandler inputHandler = new InputHandler(usage);
+    int exitCode = catchSystemExit(() ->
+        inputHandler.handleArguments(new String[] {"2", "3", "4"})
+    );
+    assertThat(exitCode).isEqualTo(1);
+    verify(usage).usageMessage();
+  }
+
+  @Test
+  public void whenFirstParameterIsALetter_thenMessageIsPrinted() throws Exception {
+    InputHandler inputHandler = new InputHandler(usage);
+    int exitCode = catchSystemExit(() ->
+        inputHandler.handleArguments(new String[] {"m", "small"})
+    );
+    assertThat(exitCode).isEqualTo(1);
+    verify(usage).usageMessage();
+  }
+
+  @Test
+  public void whenSecondParameterIsInvalid_thenMessageIsPrinted() throws Exception {
+    InputHandler inputHandler = new InputHandler(usage);
+    int exitCode = catchSystemExit(() ->
+        inputHandler.handleArguments(new String[] {"3", "toko"})
+    );
+    assertThat(exitCode).isEqualTo(1);
+    verify(usage).usageMessage();
+  }
+
+  @Test
+  public void inputHasAValidNumberOfArguments_thenArgsLengthIsTwo(){
+    InputHandler inputHandler = new InputHandler(usage);
+    Input actual = inputHandler.handleArguments(new String[] {"2", "small"});
+
+    assertThat(actual.getPlayerNumber()).isEqualTo(2);
+    assertThat(actual.getDeckSize()).isEqualTo(DeckSize.SMALL);
+  }
 }
